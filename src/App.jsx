9ed47369afce2badb501919b049d9b56e10add79
Vitorr-AF <vitoraugustofranca7@gmail.com>
2024-09-04 11:02:16 -0300

    Adicionado ao git

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom"
+import Menu from "./components/Menu"
+
+function App() {
+
+
+  return (
+    <>
+      <Menu/>
+      <Outlet />
+    </>
+  )
+}
+
+export default App
